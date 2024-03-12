05cabfc2ac2ab53eaded7314bee94c0a91b5d799
jiin <ji961118@gmail.com>
2024-03-12 14:13:58 +0900

    fix : 헤더 서브메뉴 스타일

--- resources/js/layout.js
+++ resources/js/layout.js
@@ -1,13 +1,13 @@
-let headerbar = document.querySelector('.header');
+let header = document.querySelector('.header');
 
 // document.addEventListener('scroll', () => { // 스크롤할 경우 헤드 스타일 변경
-//   (window.scrollY > 1) ? headerbar.classList.add('header--fixed') : headerbar.classList.remove('header--fixed');
+//   (window.scrollY > 1) ? header.classList.add('header--fixed') : header.classList.remove('header--fixed');
 // });
 
-headerbar.addEventListener('mouseover', () => {
-  headerbar.classList.add('open');
+header.addEventListener('mouseover', () => {
+  header.classList.add('open');
 });
 
-headerbar.addEventListener('mouseleave', () => {
-  headerbar.classList.remove('open');
+header.addEventListener('mouseleave', () => {
+  header.classList.remove('open');
 });
