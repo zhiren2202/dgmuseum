e5b281ea562924013ab28d93bf45b93f97c4242c
jiin <ji961118@gmail.com>
2024-03-13 14:31:59 +0900

    fix : 메인 - 서브메뉴 웹접근성 속성

--- resources/js/layout.js
+++ resources/js/layout.js
@@ -1,38 +1,32 @@
-let header = document.querySelector('.header');
+const header = document.querySelector('.header');
+const headerNav = document.querySelector('.header .header_navbar .lnb');
 
 // document.addEventListener('scroll', () => { // 스크롤할 경우 헤드 스타일 변경
 //   (window.scrollY > 1) ? header.classList.add('header--fixed') : header.classList.remove('header--fixed');
 // });
 
-header.addEventListener('mouseover', () => {
+headerNav.addEventListener('mouseover', () => {
   header.classList.add('open');
 });
 
-header.addEventListener('mouseleave', () => {
+headerNav.addEventListener('mouseleave', () => {
   header.classList.remove('open');
 });
 
-const btnMobile = document.querySelector('.btn_mobilemenu');
-const mobileMenu = document.querySelector(".mobile_menu");
-btnMobile.addEventListener('click', () => {
-  btnMobile.classList.toggle('close');
-  mobileMenu.classList.toggle('open');
-});
-
-function toggleMobileMenu() {
-  const clickedItem = this.parentNode;
-  const items = mobileMenu.querySelectorAll(".menu_item");
-
-  items.forEach((item) => {
-    if (clickedItem === item) {
-      item.classList.add("on");
+$(document).ready(function () {
+  // 전체메뉴 클래스명 추가/삭제 및 웹접근성 속성 추가
+  $('.header_navbar > ul li').on('click', function () {
+    if ($(this).find('a').hasClass('active')) {
+      // 클릭
+      $(this).find('a').addClass('active');
+      $(this).find('a').attr('title', '선택됨');
     } else {
-      item.classList.remove("on");
+      // 클릭하지 않은 요소 리셋
+      $(this).siblings().find('a').removeClass('active');
+      $(this).siblings().find('a').attr('title', '');
+      // 클릭
+      $(this).find('a').addClass('active');
+      $(this).find('a').attr('title', '선택됨');
     }
   });
-}
-
-const titles = mobileMenu.querySelectorAll(".menu__depth1");
-titles.forEach((title) => {
-  title.addEventListener("click", toggleMobileMenu);
 });
---
+++ resources/js/mobile_menu.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const btnMobile = document.querySelector('.btn_mobilemenu');
+const mobileMenu = document.querySelector('.mobile_menu');
+btnMobile.addEventListener('click', () => {
+  btnMobile.classList.toggle('close');
+  mobileMenu.classList.toggle('open');
+});
+
+function toggleMobileMenu() {
+  const clickedItem = this.parentNode;
+  const items = mobileMenu.querySelectorAll('.menu_item');
+
+  items.forEach((item) => {
+    if (clickedItem === item) {
+      item.classList.add('on');
+      item.setAttribute('title','선택됨');
+    } else {
+      item.classList.remove('on');
+      item.setAttribute('title','');
+    }
+  });
+}
+
+const titles = mobileMenu.querySelectorAll('.menu__depth1');
+titles.forEach((title) => {
+  title.addEventListener('click', toggleMobileMenu);
+});
+
+$(document).ready(function () {
+	// 전체메뉴 클래스명 추가/삭제 및 웹접근성 속성 추가
+	$('.menu_item ul li a').on('click', function () {
+		if ($(this).hasClass('active')) {
+			// 클릭
+			$(this).addClass('active');
+			$(this).attr('title', '선택됨');
+		} else {
+			// 클릭하지 않은 요소 리셋
+			$(this).closest('li').siblings().find('a').removeClass('active');
+			$(this).closest('li').siblings().find('a').attr('title', '');
+
+			// 클릭
+			$(this).addClass('active');
+			$(this).attr('title', '선택됨');
+		}
+	});
+});
